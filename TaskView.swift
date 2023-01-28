44dbb952c9492134dac8e9d763832c9fd3a1e8d5
youngnamh <youngnamhlady@gmail.com>
2023-01-28 12:33:34 -0500

    Added taskView

---
+++ TaskView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskView.swift
+//  TaskBully
+//
+//  Created by Bob  Jones on 2023-01-28.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
