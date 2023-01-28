6eb8577505e140a590dc4ab075713e4ea0d0ccaf
youngnamh <youngnamhlady@gmail.com>
2023-01-28 12:22:19 -0500

    i know git okay

---
+++ TaskBully/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  RootView.swift
+//  TaskBully
+//
+//  Created by Bob  Jones on 2023-01-28.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("That salsa instructor started way too fast")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
